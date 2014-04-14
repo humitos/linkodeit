99e36f5bf7515df0603fba0d26c30240c4cf9238
Manuel Kaufmann <humitos@gmail.com>
2014-04-14 18:27:59 -0300

    Structure needed by PyPI

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+
+from distutils.core import setup
+
+
+setup(
+    name='linkodeit',
+    version='0.1',
+    description='Command-line utility to publish pastes at http://linkode.org',
+    author='Manuel Kaufmann',
+    author_email='humitos@gmail.com',
+    url='https://github.com/humitos/linkodeit',
+    download_url='https://github.com/humitos/linkodeit/tarball/0.1',
+    keywords=['linkode', 'pastebin', 'paste'],
+    scripts=['linkodeit/linkodeit'],
+    classifiers=[],
+)
