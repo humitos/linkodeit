6c75884c1b42ae70036a21552be6ee561d36ca64
Manuel Kaufmann <humitos@gmail.com>
2014-04-15 02:12:01 -0300

    New version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 
 setup(
     name='linkodeit',
-    version='0.2',
+    version='0.3',
     description='Command-line utility to publish pastes at http://linkode.org',
     author='Manuel Kaufmann',
     author_email='humitos@gmail.com',
     url='https://github.com/humitos/linkodeit',
-    download_url='https://github.com/humitos/linkodeit/tarball/0.2',
+    download_url='https://github.com/humitos/linkodeit/tarball/0.3',
     keywords=['linkode', 'pastebin', 'paste'],
     scripts=['linkodeit/linkodeit'],
     install_requires=['six>=1.6.1'],
